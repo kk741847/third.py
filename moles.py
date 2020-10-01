c01c3b26b1dbcf9103c336f814c8ba873eab9a3a
kk741847 <72181282+kk741847@users.noreply.github.com>
2020-10-01 09:33:39 +0530

    Update moles.py

--- moles.py
+++ moles.py
@@ -1,6 +1,6 @@
 R = 0.08206
 P = float(input("P in atm : "))
-V = float(input("V in litre : "));
-T = float(input("T in kelvin : "));
-n = (P*V)/(R*T))
+V = float(input("V in litre : "))
+T = float(input("T in kelvin : "))
+n = (P*V)/(R*T)
 print("No. of moles in gas : ",n)
